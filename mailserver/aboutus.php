219622aaeb8589e7c4e96666115f8ebbe6c83fd7
BHUVNESH-SINGH <39794508+BHUVNESH-SINGH@users.noreply.github.com>
2018-08-18 19:26:21 +0530

    upload from computer
    project for mail

---
+++ mailserver/aboutus.php
@@ -0,0 +1,16 @@
+<h2 style="background:#99FF33;padding:5px" align="left">About us</h2>
+<p>
+IGeekS Technologies is a company located in Bangalore, India. We have being recognized as a quality provider of hardware and software solutions for the student’s in order carry out their academic Projects. We offer academic projects at various academic levels ranging from graduates to masters (Diploma, BCA, BE, M. Tech, MCA, M. Sc (CS/IT)).
+
+As a part of our vision to provide hands-on experience to the students we invite them from the stream of Electronics, Instrumentation, Computer Science & Information Science to carry out their academic project work at our facility under the guidance of industry experts.
+
+The students will be working as Project Trainees. We offer them the necessary guidance & tools to help them to complete their academic projects in the most professional way. Our focus is on developing Industry specific projects with the interaction of industry experts. Our comprehensive technical facility allows our engineering team to get results & would probably help the students to get exposed to newer tools & technologies while projects are being developed. Most of our efforts are aimed towards showcasing new technologies.
+
+IGeekS Technologies AWARDS & MILESTONES:
+
+ IGeekS Technologies trainers published several books by world class publishers like PHI India Pvt. Ltd
+
+ IGeekS Technologies trainers were recognized as the best trainers by top MNC companies like Wipro, TCS, Logical CMG, iFlex, IGate, L&T, Satyam etc.
+
+ IGeekS Technologies involved in training and academic projects program in more than 40 engineering colleges from Andhra Pradesh, Tamil Nadu, Karnataka, Uttar Pradesh and Orissa.
+</p>
---
+++ mailserver/contactus.php
@@ -0,0 +1,30 @@
+<style>
+	table{padding:5px;border-radius:5px}
+	td{padding:10px}
+</style>
+<table width="90%" border="1" align="center">
+  <tr style="background:#CCCCCC" height="30px">
+    <th colspan="2" style="color:#0000FF">Contact Us</th>
+  </tr>
+  <tr>
+    <td width="121" height="37">Name</td>
+    <td width="252">www.makefinalyearproject.com </td>
+  </tr>
+  <tr>
+    <td height="36">Mobile </td>
+    <td> 9590544567 </td>
+  </tr>
+  <tr>
+    <td>Address</td>
+    <td>Bangalore </td>
+  </tr>
+    <tr>
+    <td>Email</td>
+    <td>igeekstechnologies@gmail.com</td>
+  </tr>
+   <tr>
+    <td>Website</td>
+    <td><a href="http://www.makefinalyearproject.com">www.makefinalyearproject.com</a></td>
+  </tr>
+  
+</table>
